0b05e4e9bd01dd8aec74adbf483229a3b31e6ff9
ovpesn <126763265+ovpesn@users.noreply.github.com>
2023-08-07 05:53:23 +1000

    Created comments for Week2

--- src/Week2.java
+++ src/Week2.java
@@ -3,17 +3,22 @@ import java.awt.*;
 
 public class Week2 extends JFrame{
     // extend supposedly makes the entire class a JFrame
+    // JFrame opens a new window upon execution
+    // unlike Frame, it can close the program by clcosing the window
     public static void main(String[] args) throws Exception {
-        Week2 window = new Week2();
+        Week2 week2 = new Week2();
     }
 
     class Canvas extends JPanel {
+        // a container used to store a group of components
+        // in this case, it is used to store the grid and cells
         public Canvas() {
             setPreferredSize(new Dimension(720, 720));
         }
 
         @Override
         public void paint(Graphics g) {
+            // grants the ability to draw onto components
             g.setColor(Color.GRAY);
             g.fillRect(0, 0, 720, 720);
             for(int i = 10; i < 710; i += 35) {
@@ -31,10 +36,11 @@ public class Week2 extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // terminates the program upon closing the window
         this.setContentPane(canvas);
-        // 
+        // used to hold GUI-related objects
+        // usually used over a JPanel
         this.pack();
-        //
+        // sizes the frame so that all contents are at or above preferred sizes
         this.setVisible(true);
-        // makes the JFrame visible
+        // makes the JFrame appear on the screen
     }
 }
